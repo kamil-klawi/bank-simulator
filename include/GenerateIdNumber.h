143495828065d8bfce616d13178094aa2a5e9a14
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2025-04-02 22:13:40 +0200

    chore(utils): add an ID number generator

---
+++ include/GenerateIdNumber.h
@@ -0,0 +1,12 @@
+//
+// Created by kamil on 4/2/25.
+//
+
+#ifndef GENERATEIDNUMBER_H
+#define GENERATEIDNUMBER_H
+
+#include <string>
+
+std::string generateIdNumber(int letterCount = 5, int digitsCount = 3);
+
+#endif //GENERATEIDNUMBER_H
